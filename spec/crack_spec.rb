560dd886773402dd715a2b129bfe2e9b988a5bf5
Mark Ertmer <mark.ertmer@gmail.com>
2022-01-14 17:20:46 -0700

    Add: tests for attributes

--- spec/crack_spec.rb
+++ spec/crack_spec.rb
@@ -8,4 +8,24 @@ describe Crack do
   it '1. exists' do
     expect(@crack).to be_instance_of Crack
   end
+
+  it '2. has a collection of characters' do
+    expect(@crack.characters).to eq ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
+  end
+
+  it '3. accepts ciphertext and date' do
+    expect(@crack.ciphertext).to eq "vjqtbeaweqihssi"
+    expect(@crack.date).to eq "291018"
+  end
+
+  it '4. uses todays date by default' do
+    crack = Crack.new("vjqtbeaweqihssi")
+
+    date_string = Date::today.strftime.delete("-")
+    expected = date_string[-2..-1] + date_string[-4..-3] + date_string[-6..-5]
+
+    expect(crack.date).to eq expected
+    expect(crack.date).to eq "140122"
+  end
+
 end
