6de5be72a0f85ded46693dc2f42770a590bdfeb6
Mark Ertmer <mark.ertmer@gmail.com>
2022-01-16 14:23:47 -0700

    Add: CLI tests

--- crack.rb
+++ crack.rb
@@ -5,7 +5,7 @@ if ARGV.count != 3 ||
   (ARGV[1].include?("/") &&
   !File.exist?(ARGV[1].split("/")[0..-2].join("/"))) ||
   ARGV[2].length != 6 ||
-  ARGV[2].to_i.to_s != ARGV[2]
+  ARGV[2].to_i.to_s.rjust(6, "0") != ARGV[2]
 
   puts "INVALID INPUT\n- Make sure your source file is valid.\n- Make sure to use a valid filepath for your output.\n- Enter a date using 'DDMMYY' formatting. 6 digits, numbers only.\nCOMMAND STRUCTURE:\n$ ruby crack.rb <source filepath> <output filepath> <date>"
   exit
